4a6277229f5d24eb536fe23692d64038996eee5d
Danilo Mendes da Silva <danilo.mendess@proton.me>
2024-10-25 20:11:10 -0300

    TelaDeHome
    fim

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,31 +6,55 @@ import menu from "../../assets/icons/menu.png";
 import "./style.css";
 
 const Header = () => {
-    const menuItems:string[] = ["Home", "Noticias", "Eventos", "Negocios"];
+    const menuItems: string[] = ["Home", "Noticias", "Eventos", "Negocios"];
+    const estadosBrasileiros: string[] = [
+        "Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Distrito Federal",
+        "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul",
+        "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro",
+        "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina",
+        "São Paulo", "Sergipe", "Tocantins"
+    ];
+
+    const toggleDropdown = (event: React.MouseEvent) => {
+        const dropdown = event.currentTarget.querySelector('.dropdown');
+        if (dropdown) {
+            dropdown.classList.toggle('show');
+        }
+    };
 
     return (
         <header>
-                <img className="menu" src={menu} alt="menu hamburger" />
-                <img className="nav-banner" src={logo} alt="Logo Fala Agro" />
-                <img className="nav-logo" src={icon} alt="Fala Agro" />
-                <input className="search" placeholder="Pesquisar..." type="search" name="search" id="search" />
+            <img className="menu" src={menu} alt="menu hamburger" />
+            <img className="nav-banner" src={logo} alt="Logo Fala Agro" />
+            <img className="nav-logo" src={icon} alt="Fala Agro" />
+            <input className="search" placeholder="Pesquisar..." type="search" name="search" id="search" />
             <nav>
                 <ul>
-                    {menuItems.map(item => 
-                        <li>
-                            <Link 
-                                className="nav-link" 
-                                to={`/${item.toLocaleLowerCase()}`}>{item}
-                            </Link>
+                    {menuItems.map(item => (
+                        <li key={item} className="nav-item" onClick={item !== "Home" ? toggleDropdown : undefined}>
+                            <Link className="nav-link" to={`/${item.toLowerCase()}`}>{item}</Link>
+                            {item !== "Home" && (
+                                <ul className="dropdown">
+                                    {estadosBrasileiros.map(estado => (
+                                        <li key={estado}>
+                                            <Link to={`/${item.toLowerCase()}/${estado.toLowerCase()}`}>{estado}</Link>
+                                        </li>
+                                    ))}
+                                </ul>
+                            )}
                         </li>
-                    )}
+                    ))}
                 </ul>
             </nav>
-            <div className="user-info">
-                <img className="user-icon" src={user} alt="icone de usuario" />
+            <div className="user-container">
+                <div className="user-info">
+                    <Link to="/login">
+                        <img className="user-icon" src={user} alt="ícone de usuário" />
+                    </Link>
+                </div>
             </div>
         </header>
-    )
+    );
 }
 
 export default Header;
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,11 +1,14 @@
 import "./style.css";
 import Header from "../../components/Header";
+import Card from "../../components/Card";
 
 const HomePage = () => {
   return (
     <div className="homepage">
       <Header />
-      
+      <main className="feed">
+        {[1, 2, 3, 4, 5, 6].map((_, index) => <Card key={index} />)}
+      </main>
     </div>
   );
 };
